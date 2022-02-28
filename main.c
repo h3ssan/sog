7ccd9432eed8162d8d092c8114d26b2deb1e3db2
Hassan A Hashim <H3ssan@protonmail.com>
2022-03-01 02:03:46 +0300

    Made `generate_v3_address` function.

--- main.c
+++ main.c
@@ -1,6 +1,21 @@
 #include "main.h"
 
-int main() {
+int main(int argc, char *argv[]) {
+    // Use current time as seed for random generators
+    srand(time(0));
+
+    generate_v3_address(1);
 
     return EXIT_SUCCESS;
+}
+
+void generate_v3_address(unsigned int count) {
+    for (int j = 0; j < count; ++j) {
+        for (int i = 0; i < 55; i++) {
+            int num = (rand() % (MAX_LEN - MIN_LEN + 1)) + MIN_LEN;
+
+            printf("%c", ALPHABETS[num]);
+        }
+        printf("d.onion%c", 10);
+    }
 }
--- main.h
+++ main.h
@@ -3,5 +3,17 @@
 
 #include "stdlib.h"
 #include "stdio.h"
+#include <time.h>
+
+const unsigned char ALPHABETS[36] = { 'a', 'b', 'c', 'd', 'e', 'f', 'g',
+                                      'h', 'i', 'j', 'k', 'l', 'm', 'n',
+                                      'o', 'p', 'q', 'r', 's', 't', 'u',
+                                      'v', 'w', 'x', 'y', 'z', '0', '1',
+                                      '2', '3', '4', '5', '6', '7', '8',
+                                      '9' };
+const unsigned char MIN_LEN = 0;
+const unsigned char MAX_LEN = 35;
+
+void generate_v3_address(unsigned int count);
 
 #endif //MAIN_H
